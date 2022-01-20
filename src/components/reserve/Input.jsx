a32c137bc62ceb1c26ae5699450f67de4e54e734
shione.nagito.jr@tut.jp <shione.nagito.jr@tut.jp>
2022-01-20 17:29:08 +0900

    merge test

--- src/components/reserve/Input.jsx
+++ src/components/reserve/Input.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Input.css';
 
+//333
 export const Input = ({ value, onChange }) => {
   return <input className="reserve-input" value={value} onChange={onChange} />;
 };
