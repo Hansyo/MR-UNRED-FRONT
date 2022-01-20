1fa84116770479a771f375d6b2f4538b668b0fcc
G4RDS <account@g4rds.jp>
2022-01-20 17:15:34 +0900

    Add reserve page scaffold

--- src/pages/ReservePage.jsx
+++ src/pages/ReservePage.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import './ReservePage.css';
+import { Input } from '../components/reserve/Input';
 
 const ReservePage = () => {
-  return <div>This is ReservePage</div>;
+  const [phone, setPhone] = useState('');
+
+  const onPhoneChange = (e) => {
+    setPhone(e.target.value);
+  };
+
+  return (
+    <div>
+      <h1>This is ReservePage</h1>
+      <div className="reserve--upper-grid">
+        <div>
+          <div>{phone}</div>
+          <Input value={phone} onChange={onPhoneChange} />
+        </div>
+        <div>
+          <button>送信</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ReservePage;
