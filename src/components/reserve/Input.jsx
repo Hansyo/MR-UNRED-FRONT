c850d84af68ee6ad77423691c82224348a59d632
G4RDS <account@g4rds.jp>
2022-01-25 11:51:47 +0900

    Format

--- src/components/reserve/Input.jsx
+++ src/components/reserve/Input.jsx
@@ -5,24 +5,24 @@ import Mail from '../icons/mail.png';
 import Person from '../icons/person.png';
 import Phone from '../icons/phone.png';
 import Select from 'react-select';
-import DatePicker, { registerLocale } from "react-datepicker";
+import DatePicker, { registerLocale } from 'react-datepicker';
 import ja from 'date-fns/locale/ja';
-import "react-datepicker/dist/react-datepicker.css";
-
+import 'react-datepicker/dist/react-datepicker.css';
 
 const initialDate = new Date();
 registerLocale('ja', ja);
 
-export const Input = ({className, placeholder}) => {
-  return <input className={className} placeholder={placeholder}/>;
+export const Input = ({ className, placeholder }) => {
+  return <input className={className} placeholder={placeholder} />;
 };
 
 export const Calendar = ({ value, onChange }) => {
   return (
-    <DatePicker className="reserve-date"
+    <DatePicker
+      className="reserve-date"
       dateFormat="yyyy/MM/dd"
       timeFormat="HH:mm"
-      locale='ja'
+      locale="ja"
       selected={value}
       minDate={initialDate}
       onChange={onChange}
@@ -35,21 +35,14 @@ export const Selectbox = () => {
     { value: 'repeat', label: 'Does not repeat' },
     { value: 'daily', label: 'Daily' },
     { value: 'weekly', label: 'Weekly' },
-    { value: 'monthly', label: 'Monthly' }];
-  
-  return (
-    <Select options={options} />
-  );
+    { value: 'monthly', label: 'Monthly' },
+  ];
+
+  return <Select options={options} />;
 };
 
 export const Icon = ({ index }) => {
-  const icons = [
-    Person,
-    Phone,
-    Mail,
-    Document];
-  
-  return (
-    <img src={icons[index]} className="reserve-icon" alt="icon" />
-  );
+  const icons = [Person, Phone, Mail, Document];
+
+  return <img src={icons[index]} className="reserve-icon" alt="icon" />;
 };
