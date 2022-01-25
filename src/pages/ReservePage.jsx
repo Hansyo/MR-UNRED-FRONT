28957776e47d7b9ff705d336e76073040e0c0e26
G4RDS <account@g4rds.jp>
2022-01-25 11:54:48 +0900

    Add form states

--- src/pages/ReservePage.jsx
+++ src/pages/ReservePage.jsx
@@ -6,26 +6,43 @@ import './ReservePage.css';
 import { Calendar, Input, Selectbox, Icon } from '../components/reserve/Input';
 //import Select from 'react-select';
 
-const ReservePage = () => {
-  const [phone, setPhone] = useState('');
-  const [initDate, setDate] = useState(new Date());
-  //const [initRepeat, setRepeat] = useState('');
-  /*
-  const onPhoneChange = (e) => {
-    setPhone(e.target.value);
-    <div>
-          <div>{phone}</div>
-          <Input value={phone} onChange={onPhoneChange} />
-        </div>
-  };*/
+const Repeat = {
+  NO_REPEAT: 'NO',
+  DAILY: 'DAILY',
+  WEEKLY: 'WEEKLY',
+  MONTHLY: 'MONTHLY',
+};
 
-  const onDateChange = (date) => {
-    setDate(date);
+const ReservePage = () => {
+  const [reserveDate, setReserveDate] = useState(new Date());
+  const [reserveTimeFrom, setReserveTimeFrom] = useState('00:00');
+  const [reserveTimeTo, setReserveTimeTo] = useState('00:00');
+  const [shouldReserveAllDay, setShouldReserveAllDay] = useState(false);
+  const [reserveRepeat, setReserveRepeat] = useState(Repeat.NO_REPEAT);
+  const [userName, setUserName] = useState('');
+  const [userPhone, setUserPhone] = useState('');
+  const [userEmail, setUserEmail] = useState('');
+  const [description, setDescription] = useState('');
+  const [guestName, setGuestName] = useState('');
+  const [guestPhone, setGuestPhone] = useState('');
+  const [guestEmail, setGuestEmail] = useState('');
+
+  const sendReserve = () => {
+    console.log({
+      reserveDate,
+      reserveTimeFrom,
+      reserveTimeTo,
+      shouldReserveAllDay,
+      reserveRepeat,
+      userName,
+      userPhone,
+      userEmail,
+      description,
+      guestName,
+      guestPhone,
+      guestEmail,
+    });
   };
-  /*
-  const onRepeatChange = (repeat) => {
-    setRepeat(repeat);
-  };*/
 
   //Room Name，電話番号，メールアドレスは自動で入る？
   return (
@@ -33,38 +50,63 @@ const ReservePage = () => {
       <div className="reserve--upper-grid">
         <h1>Room Name</h1>
         <div>
-          <a type="submit" class="reserve-saveButton reserve-saveButton--red ">
+          <button
+            class="reserve-saveButton reserve-saveButton--red"
+            onClick={sendReserve}
+          >
             SAVE
-          </a>
+          </button>
         </div>
       </div>
       <hr className="reserve-line"></hr>
       <div className="reserve--upper-grid">
         <div>
-          <Calendar value={initDate} onChange={onDateChange} />
+          <Calendar value={reserveDate} onChange={setReserveDate} />
         </div>
         <div>
-          <input type="time" class="reserve-time" required></input>
+          <input
+            value={reserveTimeFrom}
+            onChange={(e) => setReserveTimeFrom(e.target.value)}
+            type="time"
+            class="reserve-time"
+            required
+          />
         </div>
 
         <div>
-          <input type="time" class="reserve-time" required></input>
+          <input
+            value={reserveTimeTo}
+            onChange={(e) => setReserveTimeTo(e.target.value)}
+            type="time"
+            class="reserve-time"
+            required
+          />
         </div>
       </div>
 
       <div className="reserve--upper-grid">
         {/* <input id="allDayTag" value="1" type="checkbox" /><label for="allDayTag"> All Day </label>  */}
         <label class="ECM_CheckboxInput">
-          <input class="ECM_CheckboxInput-Input" type="checkbox"></input>
+          <input
+            value={shouldReserveAllDay}
+            onChange={(e) => setShouldReserveAllDay(e.target.checked)}
+            class="ECM_CheckboxInput-Input"
+            type="checkbox"
+          />
           <span class="ECM_CheckboxInput-DummyInput"></span>
           <span class="ECM_CheckboxInput-LabelText">ALL DAY</span>
         </label>
         <div class="ipselect">
-          <select class="SB" required>
-            <option value="" hidden disabled selected></option>
-            <option value="1">daily</option>
-            <option value="2">weekly</option>
-            <option value="3">monthly</option>
+          <select
+            value={reserveRepeat}
+            onChange={(e) => setReserveRepeat(e.target.value)}
+            class="SB"
+            required
+          >
+            <option value={Repeat.NO_REPEAT} hidden disabled selected></option>
+            <option value={Repeat.DAILY}>daily</option>
+            <option value={Repeat.WEEKLY}>weekly</option>
+            <option value={Repeat.MONTHLY}>monthly</option>
           </select>
           <span class="SB_highlight"></span>
           <span class="SB_selectbar"></span>
@@ -85,11 +127,21 @@ const ReservePage = () => {
           <Icon index={0} />
           <div class="flexbox">
             <a>User Name</a>
-            <Input className="reserve--event-input" placeholder="User Name" />
+            <Input
+              value={userName}
+              onChange={setUserName}
+              className="reserve--event-input"
+              placeholder="User Name"
+            />
           </div>
           <div class="flexbox">
             <a>Guest Name</a>
-            <Input className="reserve--guest-input" placeholder="User Name" />
+            <Input
+              value={guestName}
+              onChange={setGuestName}
+              className="reserve--guest-input"
+              placeholder="User Name"
+            />
           </div>
         </div>
       </div>
@@ -99,11 +151,18 @@ const ReservePage = () => {
           <div class="flexbox">
             <a>Phone Number</a>
             <Input
+              value={userPhone}
+              onChange={setUserPhone}
               className="reserve--event-input"
               placeholder="090-1234-5678"
             />
           </div>
-          <Input className="reserve--guest-input" placeholder="090-1234-5678" />
+          <Input
+            value={guestPhone}
+            onChange={setGuestPhone}
+            className="reserve--guest-input"
+            placeholder="090-1234-5678"
+          />
         </div>
       </div>
 
@@ -112,11 +171,15 @@ const ReservePage = () => {
         <div class="flexbox">
           <a>E-Mail</a>
           <Input
+            value={userEmail}
+            onChange={setUserEmail}
             className="reserve--event-input"
             placeholder="info@example.com"
           />
         </div>
         <Input
+          value={guestEmail}
+          onChange={setGuestEmail}
           className="reserve--guest-input"
           placeholder="info@example.com"
         />
@@ -125,6 +188,8 @@ const ReservePage = () => {
       <div className="reserve--event-grid">
         <Icon index={3} />
         <Input
+          value={description}
+          onChange={setDescription}
           className="reserve--event-input"
           placeholder="Add purpose and description"
         />
