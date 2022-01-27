dd39f1f89cdc635c2c372715dfc768261c45abdc
00000da <oda.manato.dev@gmail.com>
2022-01-27 10:02:21 +0900

    入力画面の下左側の書式を変更

--- src/pages/ReservePage.jsx
+++ src/pages/ReservePage.jsx
@@ -31,13 +31,13 @@ const ReservePage = () => {
 
   //Room Name，電話番号，メールアドレスは自動で入る？
   return (
-  
+    // 入力フォーム
     <div>
       {/* 部屋の名前 */}
       <div className="reserve--upper-grid">
       <h1>Room Name</h1>
         <div>
-          <a type="submit" class="reserve-saveButton reserve-saveButton--red ">SAVE</a>
+          <a type="submit" className="reserve-saveButton reserve-saveButton--red ">SAVE</a>
         </div>
       </div>
       <hr className='reserve-line'></hr>
@@ -48,89 +48,89 @@ const ReservePage = () => {
           <Calendar value={initDate} onChange={onDateChange} />
         </div>
         <div>
-          <input type="time" class="reserve-time" required></input>
+          <input type="time" className="reserve-time" required></input>
         </div> 
         <div>
-          <input type="time" class="reserve-time" required></input>
+          <input type="time" className="reserve-time" required></input>
         </div>
       </div> 
 
       {/* DAYChoose */}
       <div className="reserve--upper-DAY">
-        <label class="ECM_CheckboxInput">
-          <input class="ECM_CheckboxInput-Input" type="checkbox"></input><span class="ECM_CheckboxInput-DummyInput"></span><span class="ECM_CheckboxInput-LabelText">ALL DAY</span>
+        <label className="ECM_CheckboxInput">
+          <input className="ECM_CheckboxInput-Input" type="checkbox"></input><span class="ECM_CheckboxInput-DummyInput"></span><span class="ECM_CheckboxInput-LabelText">ALL DAY</span>
         </label>
-        <div class="ipselect">
-          <select class="SB" required>
+        <div className="ipselect">
+          <select className="SB" required>
           <option value="" hidden disabled selected></option>
           <option value="1">daily</option>
           <option value="2">weekly</option>
           <option value="3">monthly</option>
           </select>
-          <span class="SB_highlight"></span>
-          <span class="SB_selectbar"></span>
-          <label class="SB_selectlabel">Choose</label>
+          <span className="SB_highlight"></span>
+          <span className="SB_selectbar"></span>
+          <label className="SB_selectlabel">Choose</label>
         </div>
       </div>
 
+      {/* 予約者情報入力 */}
+      <div className="reserve--event-grid">
+        {/* 教員用入力画面 */}
+        <div className='reserve--event-user'>
+          <div className='reserve--event-usertitle'>
+            <h1> Event Details</h1> 
+            <hr className='reserve-line'></hr><br></br>
+          </div>
+
+          <div className='reserve--event-form'>
+            <Icon index={0} />
+            <div className="flexbox">
+              <a>User Name</a>
+              <Input className="reserve--event-input" placeholder="User Name" />
+            </div>
+          </div>
+
+          <div className='reserve--event-form'>
+            <Icon index={1} />
+            <div className="flexbox">
+              <a>Phone Number</a>
+              <Input className="reserve--event-input" placeholder="012-3456-7890" />
+            </div>
+          </div>
+
+          <div className='reserve--event-form'>
+          <Icon index={2} />
+          <div className="flexbox">
+            <a>E-Mail</a>
+            <Input className="reserve--event-input" placeholder="info@example.com" />
+          </div>
+          </div>
+
+          <div className='reserve--event-form'>
+          <Icon index={3} />
+          <div className="flexbox">
+            <a>Purpose and Description</a>
+            <Input className="reserve--event-input" placeholder="Add purpose and description" />
+          </div>
+          </div>
+        
+        </div> 
+        {/* ゲスト用入力画面 */}
+        <div className='reserve--event-guest'>
+          <div className='reserve--event-usertitle'>
+            <h1> Guest Details</h1> 
+            <hr className='reserve-line'></hr><br></br>
+          </div>
+          <div className='reserve--event-form'>
+            <div className="flexbox">
+              <a>detailed information</a>
+              <Input type="text" className="reserve--event-input-typetext" placeholder="補足" />
+            </div>
+          </div>
+        </div>
+
+      </div>
       
-
-
-
-      
-      <div> 
-        <div className="reserve--event-grid"><h1> Event Details</h1> <h1 className="reserve--guest-grid">Guests Details</h1></div>
-        <hr className='reserve-line'></hr><br></br>
-      </div>
-      <div>
-        <div className='reserve--event-grid'>
-          <Icon index={0} />
-          <div class="flexbox">
-            <a>User Name</a>
-            <Input className="reserve--event-input" placeholder="User Name" />
-          </div>
-          <div class="flexbox">
-            <a>Guest Name</a>
-            <Input className="reserve--guest-input" placeholder="User Name"/>
-          </div>
-
-        </div>
-
-      </div>
-      <div>
-        <div className='reserve--event-grid'>
-          <Icon index={1} />
-          <div class="flexbox">
-            <a>Phone Number</a>
-            <Input className="reserve--event-input" placeholder="090-1234-5678" />
-          </div>
-          <div class="flexbox">
-            <a>Guest Number</a>
-            <Input className="reserve--guest-input" placeholder="090-1234-5678"/>
-          </div>
-        </div>
-      </div>
-
-
-      <div className='reserve--event-grid'>
-        <Icon index={2} />
-        <div class="flexbox">
-          <a>E-Mail</a>
-          <Input className="reserve--event-input" placeholder="info@example.com" />
-        </div>
-        <div class="flexbox">
-          <a>Guest E-Mail</a>
-          <Input className="reserve--guest-input" placeholder="info@example.com"/>
-        </div>
-      </div>
-
-      <div className='reserve--event-grid'>
-        <Icon index={3} />
-        <div class="flexbox">
-          <a>Purpose and Description</a>
-          <Input className="reserve--event-input" placeholder="Add purpose and description" />
-        </div>
-      </div>
     </div>
   );
 };
