244c2183cb19f2f9a54ce832b67c0b72c1a6da3c
00000da <oda.manato.dev@gmail.com>
2022-01-31 12:59:19 +0900

    日付のボタンをスクロールしても残っているようにした

--- src/pages/ReservationsDailyPage.jsx
+++ src/pages/ReservationsDailyPage.jsx
@@ -1,14 +1,12 @@
 import React, { useState } from 'react';
 import { DailyCalendar } from '../components/reservations/daily/DailyCalendar/DailyCalendar';
-import { DateSwitcher } from '../components/reservations/daily/DateSwitcher';
 
 const ReservationsDailyPage = () => {
   const [selectedDate, setSelectedDate] = useState(new Date());
 
   return (
     <div className="reservations-daily--page">
-      <DateSwitcher selectedDate={selectedDate} onChange={setSelectedDate} />
-      <DailyCalendar reservations={[]} />
+      <DailyCalendar reservations={[]} selectedDate={selectedDate} onDateChange={setSelectedDate} />
     </div>
   );
 };
