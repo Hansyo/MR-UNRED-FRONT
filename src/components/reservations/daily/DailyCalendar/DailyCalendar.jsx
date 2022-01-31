b8c2bfd610d7efb8eb9640112bae5bcdfced9f13
00000da <oda.manato.dev@gmail.com>
2022-01-31 12:59:19 +0900

    日付のボタンをスクロールしても残っているようにした

--- src/components/reservations/daily/DailyCalendar/DailyCalendar.jsx
+++ src/components/reservations/daily/DailyCalendar/DailyCalendar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './DailyCalendar.css';
 import { DailyCalendarRoomColumn } from './DailyCalendarRoomColumn';
+import { DateSwitcher } from '../DateSwitcher';
 
 const times = new Array(24)
   .fill()
@@ -85,18 +86,19 @@ const dummyRooms = [
   },
 ];
 
-export const DailyCalendar = ({ rooms = dummyRooms }) => {
+export const DailyCalendar = ({ rooms = dummyRooms, selectedDate, onDateChange }) => {
   return (
     <div className="daily-calendar">
       <div className="daily-calendar--header">
-        <div className="daily-calendar--time-spacer" />
-        {rooms.map((room) => (
-          <div className='daily-calendar--room-labels'>
-            <div className="daily-calendar--room-label" key={room.id}>
+        <DateSwitcher selectedDate={selectedDate} onChange={onDateChange} />
+        <div className='daily-calendar--header-room'>
+          <div className="daily-calendar--time-spacer" />
+          {rooms.map((room) => (
+            <div className="daily-calendar--room-label"key={room.id}>
               {room.name}
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
       <div className="daily-calendar--body">
         <div className="daily-calendar--times">
