3356e5f9fe9791cf315a60ed16772a0f39a9a9a7
G4RDS <account@g4rds.jp>
2022-01-31 11:38:16 +0900

    Include seconds

--- src/apis/reserve.js
+++ src/apis/reserve.js
@@ -9,8 +9,8 @@ export const postReserve = async (
   guestDetail,
 ) => {
   await requestPost('/reserve', {
-    start_date_time: startDateTime.toISOString().slice(0, 16),
-    end_date_time: endDateTime.toISOString().slice(0, 16),
+    start_date_time: startDateTime.toISOString().slice(0, 19), // 2022-01-01T00:00:00
+    end_date_time: endDateTime.toISOString().slice(0, 19), // 2022-01-01T00:00:00
     reserver_name: reserverName,
     purpose,
     guest_name: guestName,
