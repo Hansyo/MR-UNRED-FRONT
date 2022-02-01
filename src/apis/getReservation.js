401b59375d020f6572be966c4cada524af916a1d
tut203331 <shione.nagito.jr@tut.jp>
2022-02-01 09:17:15 +0900

    Use time.toISOString

--- src/apis/getReservation.js
+++ src/apis/getReservation.js
@@ -6,8 +6,8 @@ export const getReserve = async (
     roomId,
 ) => {
     const params = {
-        start_date_time: startDateTime,
-        end_date_time: endDateTime,
+        start_date_time: startDateTime.toISOString().slice(0, 19),
+        end_date_time: endDateTime.toISOString().slice(0, 19),
         room_Id: roomId
     };
     console.log({
