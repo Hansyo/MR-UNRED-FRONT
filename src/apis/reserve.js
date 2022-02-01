d40aacd53587069abcea2b0edac3ae7482766e4b
G4RDS <account@g4rds.jp>
2022-02-01 11:38:46 +0900

    Fix payload format

--- src/apis/reserve.js
+++ src/apis/reserve.js
@@ -9,12 +9,12 @@ export const postReserve = async (
   guestDetail,
 ) => {
   await requestPost('/reserve', {
-    start_date_time: startDateTime.toISOString().slice(0, 19), // 2022-01-01T00:00:00
-    end_date_time: endDateTime.toISOString().slice(0, 19), // 2022-01-01T00:00:00
+    start_date_time: startDateTime.toISOString(),
+    end_date_time: endDateTime.toISOString(),
     reserver_name: reserverName,
     purpose,
     guest_name: guestName,
     guest_detail: guestDetail,
-    room_id: 0, // TODO: roomIdをセット
+    room_id: 1, // TODO: roomIdをセット
   });
 };
