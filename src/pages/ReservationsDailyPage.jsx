94ee409165ab8c230c7ea592911bb5ffaca10bdb
tut203331 <shione.nagito.jr@tut.jp>
2022-02-04 08:53:49 +0900

    日毎の予約を確認できるuseStateを追加

--- src/pages/ReservationsDailyPage.jsx
+++ src/pages/ReservationsDailyPage.jsx
@@ -2,13 +2,15 @@ import React, { useState } from 'react';
 import { DailyCalendar } from '../components/reservations/daily/DailyCalendar/DailyCalendar';
 import { DateSwitcher } from '../components/reservations/daily/DateSwitcher';
 
+
 const ReservationsDailyPage = () => {
   const [selectedDate, setSelectedDate] = useState(new Date());
-
+  const [dateData, setDateData] = useState([]);
+  
   return (
     <div className="reservations-daily--page">
-      <DateSwitcher selectedDate={selectedDate} onChange={setSelectedDate} />
-      <DailyCalendar reservations={[]} />
+      <DateSwitcher selectedDate={selectedDate} onChange={setSelectedDate}onChangeData = { setDateData }/> 
+      <DailyCalendar reservations={dateData} />
     </div>
   );
 };
