7f97544f89f552f37e9175788afbabd14ad28b5f
tut203331 <shione.nagito.jr@tut.jp>
2022-02-07 13:47:28 +0900

    Delete split and join

--- src/apis/getReservation.js
+++ src/apis/getReservation.js
@@ -10,7 +10,7 @@ export const getReserve = (
         end_date_time: endDateTime.toISOString(),
         room_id: roomId
     };
-    /* URLSearchParamsは':'が'%3A'になるので注意 */
-    const query_params = ((new URLSearchParams(params)).toString()).split('%3A');
-    return requestGet('/reserve/?' + query_params.join(':'));
+    
+    const query_params = new URLSearchParams(params).toString();;  
+    return requestGet('/reserve/?' + query_params);
   };
