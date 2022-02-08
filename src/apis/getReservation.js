ebab34bcbe5be199200bcf303a1dfaffd377d643
G4RDS <account@g4rds.jp>
2022-02-08 11:07:38 +0900

    Fix GET reservation api

--- src/apis/getReservation.js
+++ src/apis/getReservation.js
@@ -1,16 +1,14 @@
 import { requestGet } from './request';
 
-export const getReserve = (
-    startDateTime,
-    endDateTime,
-    roomId,
-) => {
-    const params = {
-        start_date_time: startDateTime.toISOString(),
-        end_date_time: endDateTime.toISOString(),
-        room_id: roomId
-    };
-    
-    const query_params = new URLSearchParams(params).toString();;  
-    return requestGet('/reserve/?' + query_params);
+export const getReserve = (startDateTime, endDateTime, roomId) => {
+  const params = {
+    start_date_time: startDateTime.toISOString(),
+    end_date_time: endDateTime.toISOString(),
   };
+  if (roomId) {
+    params.roomId = roomId;
+  }
+
+  const query_params = new URLSearchParams(params).toString();
+  return requestGet('/reserve/?' + query_params);
+};
