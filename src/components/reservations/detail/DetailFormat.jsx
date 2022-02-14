e65fa2a19ed24b9d3a276c600329b66b0b564ebd
tut203331 <shione.nagito.jr@tut.jp>
2022-02-15 08:59:30 +0900

    Add window.confirm()

--- src/components/reservations/detail/DetailFormat.jsx
+++ src/components/reservations/detail/DetailFormat.jsx
@@ -2,13 +2,26 @@ import React from 'react';
 import './DetailFormat.css';
 
 export const DetailFormat = ({ detailData, repetitionData }) => {
-
+    console.log(detailData);
     const roomName = ["会議室1", "会議室2", "会議室3", "会議室4", "会議室5", "会議室6", "会議室7", "会議室8"];
     const listItems = repetitionData.map((data) => (
-        <div>
+        <div key={data.id}>
             {`${((data.start_date_time).slice(0,19)).replace('T','/')} - ${((data.end_date_time).slice(0,19)).replace('T','/')}`} 
         </div>
     ));
+
+    const deleteSingleReservations = () => {
+        if (window.confirm("この予約を削除してもよろしいですか？")) {
+            console.log("一日だけdeleteするよ");
+      }
+    };
+
+    const deleteMultipleReservations = () => {
+        if (window.confirm("すべての予約削除してもよろしいですか？")) {
+            console.log("すべてdeleteするよ"); /*未実装*/
+      }
+    };
+
     return (
         <div className="reservations-detail--grid">
             <div className="reservations-detail--container">
@@ -19,14 +32,16 @@ export const DetailFormat = ({ detailData, repetitionData }) => {
                 <div className="reservations-detail--label"> 日時</div>
                 <div className="reservations-detail--data">{`${((detailData.start_date_time).slice(0,19)).replace('T','/')} - ${((detailData.end_date_time).slice(0,19)).replace('T','/')}`} </div>
                 <div>
-                    <button className="reservations-detail--button"> 削除 </button>
+                    <button className="reservations-detail--button"
+                    onClick={deleteSingleReservations}> 削除 </button>
                 </div>
             </div>
             <div className="reservations-detail--container">
                 <div className="reservations-detail--label"> 一覧</div>
                 <div className="reservations-detail--data">{listItems}</div>
                 <div>
-                    <button className="reservations-detail--button"> すべて削除 </button>
+                    <button className="reservations-detail--button"
+                        onClick={deleteMultipleReservations}> すべて削除 </button>
                 </div>
             </div>
             <div className="reservations-detail--container">
