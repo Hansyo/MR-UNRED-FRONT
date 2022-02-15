5a42505e66dfff6e00f713a422b0be63b666926d
G4RDS <account@g4rds.jp>
2022-02-15 16:49:02 +0900

    Remove unused codes

--- src/components/reservations/weekly/WeeklyCalendar/WeeklyCalendarReservationItem.jsx
+++ src/components/reservations/weekly/WeeklyCalendar/WeeklyCalendarReservationItem.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const ROW_HEIGHT_PX = 64;
-
 const dateToHourAndMinute = (date) => {
-  return `${('00' + date.getHours()).slice(-2)}:${('00' + date.getMinutes()).slice(-2)}`;
+  return `${('00' + date.getHours()).slice(-2)}:${(
+    '00' + date.getMinutes()
+  ).slice(-2)}`;
 };
 
 export const WeeklyCalendarReservationItem = ({
@@ -13,15 +13,13 @@ export const WeeklyCalendarReservationItem = ({
   reserverName,
 }) => {
   const startDate = new Date(startDateTime);
-  console.log(startDate)
   const endDate = new Date(endDateTime);
-  const periodStr = `${dateToHourAndMinute(startDate)} ~ ${dateToHourAndMinute(endDate,)
-  }`;
+  const periodStr = `${dateToHourAndMinute(startDate)} ~ ${dateToHourAndMinute(
+    endDate,
+  )}`;
 
   return (
-    <div
-      className="weekly-calendar--reservation-item"
-    >
+    <div className="weekly-calendar--reservation-item">
       {periodStr}
       {`（${guestName || reserverName}）`}
     </div>
