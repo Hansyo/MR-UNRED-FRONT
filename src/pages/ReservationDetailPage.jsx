609587b41406a74b3963855193194f8cc3e5542d
tut203331 <shione.nagito.jr@tut.jp>
2022-02-15 09:00:42 +0900

    change usestate

--- src/pages/ReservationDetailPage.jsx
+++ src/pages/ReservationDetailPage.jsx
@@ -1,17 +1,23 @@
 import React, { useState, useEffect } from 'react';
+import { useLayoutEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { getDetails } from '../apis/getDetails';
 import { DetailFormat } from '../components/reservations/detail/DetailFormat';
 
 const ReservationDetailPage = () => {
   const { reservationId } = useParams();
-  const [detailData, setDetailData] = useState({});
+  
+  const [detailData, setDetailData] = useState({
+      start_date_time: "                ",
+      end_date_time: "                ",
+  });
+  
   const [repetitionData, setRepetitionData] = useState([]);
 
   const receiveDetails = async () => {
     setDetailData(await getDetails(reservationId));
-  };
-
+    };
+   
   //TODO 繰り返し予約の取得
   const dumyrep = [
     {
@@ -39,27 +45,14 @@ const ReservationDetailPage = () => {
       updated_at: "2022-02-10T07:17:06.000000Z"
     },
   ];
-
-  const dumy = {
-      id: 1,
-      guest_name: "TEST USER",
-      start_date_time: "2022-02-02T08:00:00.000000Z",
-      end_date_time: "2022-02-02T09:00:00.000000Z",
-      purpose: "purpose",
-      guest_detail: "User",
-      room_id: 1,
-      repitation_id: 1,
-      created_at: "2022-02-10T07:17:06.000000Z",
-      updated_at: "2022-02-10T07:17:06.000000Z"
-  };
   
   useEffect(() => {
     receiveDetails();
-  }, []);
+  },[]);
   
   return (
     <div className="reservations-detail--page">
-      <DetailFormat detailData={dumy} repetitionData={dumyrep} />
+      <DetailFormat detailData={detailData} repetitionData={dumyrep} />
     </div>
   );
 };
