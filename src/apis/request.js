ca103c7f62645fcfe9bd63ff629a80ac0defdfb9
tut203331 <shione.nagito.jr@tut.jp>
2022-02-15 09:03:17 +0900

    Return header on POST

--- src/apis/request.js
+++ src/apis/request.js
@@ -3,6 +3,9 @@ const BASE_URL = 'http://localhost/api';
 export const requestPost = async (path, body) => {
   const res = await fetch(`${BASE_URL}${path}`, {
     method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
     body: JSON.stringify(body),
   });
   if (!res.ok) {
