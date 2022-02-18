a94988f0588790bbcac313833034a0a9f2730eb6
tut203331 <shione.nagito.jr@tut.jp>
2022-02-18 12:04:31 +0900

    Resolve timezone

--- src/pages/ReservationDetailPage.jsx
+++ src/pages/ReservationDetailPage.jsx
@@ -2,20 +2,28 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { getDetails } from '../apis/getDetails';
 import { DetailFormat } from '../components/reservations/detail/DetailFormat';
-import { getRepeatReservation } from '../apis/getRepeatReservation';
+import { format } from 'date-fns';
 
 const ReservationDetailPage = () => {
   const { reservationId } = useParams();
-  const [detailData, setDetailData] = useState({
-      start_date_time: "                ",
-      end_date_time: "                ",
-  });
+  const [detailData, setDetailData] = useState([]);
   const [repetitionData, setRepetitionData] = useState([]);
 
   useEffect(() => {
     const receiveDetail = async () => {
-      const detail = await getDetails(reservationId);
-      setDetailData(detail);
+        const detail = await getDetails(reservationId);
+        const arrangeDetail = {
+          id: detail.id,
+          startDateTime: format(new Date(detail.start_date_time), 'yyyy-MM-dd HH:mm'),
+          endDateTime: format(new Date(detail.end_date_time),'yyyy-MM-dd HH:mm'),
+          reserverName: detail.reserver_name,
+          guestName: detail.guest_name,
+          guestDetail: detail.guest_detail,
+          purpose: detail.purpose,
+          roomName: detail.room.name,
+          repitationId: detail.repitation_id,
+        };
+        setDetailData(arrangeDetail);
     };
     receiveDetail();
   },[]);
