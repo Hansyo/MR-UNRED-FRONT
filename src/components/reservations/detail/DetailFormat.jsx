734e413dd2fb6053af4bb662008598e55a635a51
tut203331 <shione.nagito.jr@tut.jp>
2022-02-20 02:15:52 +0900

    繰り返し表示関係を調整

--- src/components/reservations/detail/DetailFormat.jsx
+++ src/components/reservations/detail/DetailFormat.jsx
@@ -1,98 +1,77 @@
 import React from 'react';
-import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { deleteReserve } from '../../../apis/delete';
-import { getRepeatReservation } from '../../../apis/getRepeatReservation';
-import { format } from 'date-fns';
 import './DetailFormat.css';
 
-export const DetailFormat = ({ detailData, repetitionData, setRepetitionData }) => {
+export const DetailFormat = ({ detailData, repetitionData}) => {
     const navigate = useNavigate();
     const currentDate = new Date();
 
-    useEffect(() => {
-        const receiveRepeatDetail = async () => {
-            try {
-                const rawRepDetails = await getRepeatReservation(detailData.repitationId);
-                const repDetails = rawRepDetails.map((repDetail) => ({
-                    id: repDetail.id,
-                    startDateTime: format(new Date(repDetail.start_date_time), 'yyyy-MM-dd HH:mm'),
-                    endDateTime: format(new Date(repDetail.end_date_time), 'yyyy-MM-dd HH:mm'),
-                }));
-                setRepetitionData(repDetails);  
-            } catch (err) {
-            }
-        };
-        receiveRepeatDetail();
-    },[detailData]);
-    
     const deleteSingleReservations = async () => {
         if (window.confirm("この予約を削除してもよろしいですか？")) {
-            try {
-                await deleteReserve(detailData.id, false);
-                alert('削除しました');
-                navigate(`./../monthly`);  
-            } catch (err) {
-                alert(`保存に失敗しました：${err.message}`);
-            }
+            await deleteReserve(detailData.id, false);
+            alert('削除しました');
+            navigate(`./../weekly`);
         }
     };
 
     const deleteRepeatReservations = async () => {
         if (window.confirm("すべての予約削除してもよろしいですか？")) {
-            try {
-                await deleteReserve(detailData.id, true);
-                alert('削除しました');
-                navigate(`./../monthly`);  
-            } catch (err) {
-                alert(`保存に失敗しました：${err.message}`);
-            }
+            await deleteReserve(detailData.id, true);
+            alert('削除しました');
+            navigate(`./../weekly`);
         }
     };
 
     const viewSingleDate = () => {
-        if (detailData.startDateTime) {
-            const listItems = [
-                <div className="reservations-detail--data">
-                    {`${detailData.startDateTime} - ${detailData.endDateTime}`}
+        const repeatItems = [
+            <div className="reservations-detail--data">
+                {`${detailData.startDateTime} - ${detailData.endDateTime}`}
+            </div>
+        ];
+        /* 予定の開始時刻が現在の時刻以降の場合のみ，削除ボタンが表示される */
+        if (new Date(detailData.startDateTime) > currentDate.getTime()) {
+            repeatItems.push(
+                <div>
+                    <button className="reservations-detail--button"
+                        onClick={deleteSingleReservations}>削除</button>
                 </div>
-            ];
-            if (new Date(detailData.startDateTime) > currentDate.getTime()) {
-                listItems.push(
-                    <div>
-                        <button className="reservations-detail--button"
-                            onClick={deleteSingleReservations}>削除</button>
-                    </div>
-                );
-            }
-            return listItems;
+            );
         }
+        return repeatItems;
     };
 
     const viewRepeatDate = () => {
-        if (repetitionData.length > 1) {
-            const listItems = [
+        /* 繰り返し予定の開始時刻が現在の時刻以降の場合のみ表示される
+        ただし，残りの繰り返し予定が現在表示している予定だけの場合は表示されない */
+        if (new Date(repetitionData.slice(-1)[0].startDateTime) > currentDate.getTime()) {
+            let checkRepeatDate = false;
+            const repeatItems = [
                 <div className="reservations-detail--data">
                     {repetitionData.map((data) => {
                         if (new Date(data.startDateTime) > currentDate.getTime()) {
-                            return (
-                                <div key={data.id}>
-                                    {`${data.startDateTime} - ${data.endDateTime}`}
-                                </div>
-                            );
+                            if (data.id !== detailData.id) {
+                                checkRepeatDate = true;
+                            }
+                            if (checkRepeatDate) {
+                                return (
+                                    <div>
+                                        {`${data.startDateTime} - ${data.endDateTime}`}
+                                    </div>
+                                );
+                            }
                         }
                     })}
                 </div>
             ];
-            if (new Date(repetitionData.slice(-1)[0].startDateTime) > currentDate.getTime()) {
-                listItems.push(
+            if (checkRepeatDate)
+                repeatItems.push(
                     <div>
                         <button className="reservations-detail--button"
                             onClick={deleteRepeatReservations}>すべて削除</button>
                     </div>
                 );
-            }
-            return listItems;
+            return repeatItems;
         }
     };
 
@@ -127,6 +106,6 @@ export const DetailFormat = ({ detailData, repetitionData, setRepetitionData }) 
                 <div className="reservations-detail--data">{detailData.purpose}</div>
             </div>
         </div>
-        
+
     );
 };
--- src/pages/ReservationDetailPage.jsx
+++ src/pages/ReservationDetailPage.jsx
@@ -1,43 +1,62 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { getDetails } from '../apis/getDetails';
 import { DetailFormat } from '../components/reservations/detail/DetailFormat';
 import { format } from 'date-fns';
 import { Header } from '../components/common/Header/Header';
+import { getRepeatReservation } from '../apis/getRepeatReservation';
 
 const ReservationDetailPage = () => {
   const { reservationId } = useParams();
-  const [detailData, setDetailData] = useState([]);
-  const [repetitionData, setRepetitionData] = useState([]);
+  const [detailData, setDetailData] = useState({ id: -1 });
+  const [repetitionData, setRepetitionData] = useState([{}]);
   const navigate = useNavigate();
+  const ref = useRef(true);
   useEffect(() => {
     const receiveDetail = async () => {
-        const detail = await getDetails(reservationId);
-        const arrangeDetail = {
-          id: detail.id,
-          startDateTime: format(new Date(detail.start_date_time), 'yyyy-MM-dd HH:mm'),
-          endDateTime: format(new Date(detail.end_date_time),'yyyy-MM-dd HH:mm'),
-          reserverName: detail.reserver_name,
-          guestName: detail.guest_name,
-          guestDetail: detail.guest_detail,
-          purpose: detail.purpose,
-          roomName: detail.room.name,
-          repitationId: detail.repitation_id,
-        };
-        setDetailData(arrangeDetail);
+      const detail = await getDetails(reservationId);
+      const arrangeDetail = {
+        id: detail.id,
+        startDateTime: format(new Date(detail.start_date_time), 'yyyy-MM-dd HH:mm'),
+        endDateTime: format(new Date(detail.end_date_time), 'yyyy-MM-dd HH:mm'),
+        reserverName: detail.reserver_name,
+        guestName: detail.guest_name,
+        guestDetail: detail.guest_detail,
+        purpose: detail.purpose,
+        roomName: detail.room.name,
+        repitationId: detail.repitation_id,
+      };
+      setDetailData(arrangeDetail);
     };
     receiveDetail();
-  },[]);
+  }, []);
 
+  useEffect(() => {
+    const receiveRepeatDetail = async () => {
+        const rawRepDetails = await getRepeatReservation(detailData.repitationId);
+        const repDetails = rawRepDetails.map((repDetail) => ({
+            id: repDetail.id,
+            startDateTime: format(new Date(repDetail.start_date_time), 'yyyy-MM-dd HH:mm'),
+            endDateTime: format(new Date(repDetail.end_date_time), 'yyyy-MM-dd HH:mm'),
+        }));
+        setRepetitionData(repDetails);
+    };
+    if (ref.current) {
+        ref.current = false;
+        return;
+    }
+    receiveRepeatDetail();
+  }, [detailData]);
+  
   return (
     <div>
       <Header />
       <div className="reservations-detail--page">
-        <a class="reservations-detail--back" href="#"
+        <a className="reservations-detail--back" href="#"
           onClick={() => { navigate(-1); return false; }}>戻る</a>
-        <DetailFormat detailData={detailData} repetitionData={repetitionData} setRepetitionData={setRepetitionData} />
+        <DetailFormat detailData={detailData} repetitionData={repetitionData} />
       </div>
-    </div> 
+    </div>
   );
 };
 export default ReservationDetailPage;
