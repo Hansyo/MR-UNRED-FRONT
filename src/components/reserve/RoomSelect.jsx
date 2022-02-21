b02506017474f8f4a52f157fffbf8b0843171380
tut171836 <tsuji.satoshi.se@tut.jp>
2022-02-21 16:51:59 +0900

    [fix]RoomSelectのmap optionにkeyを追加

--- src/components/reserve/RoomSelect.jsx
+++ src/components/reserve/RoomSelect.jsx
@@ -7,7 +7,7 @@ export const RoomSelect = ({ rooms, setRoomid }) => {
     <div className="reserve--upper-grid-roomname" onChange={(e) => setRoomid(e.target.value)}>
         <select className="room-select" required>
             {rooms.map((room) => (
-                <option className="room-select-item" value={room.id}>{room.name}</option>
+                <option className="room-select-item" value={room.id} key={room.id}>{room.name}</option>
             ))}
         </select>
     </div>
