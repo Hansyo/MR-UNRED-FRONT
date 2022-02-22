e9ad7be5bf0f1347af63bdba67e5dc215aa484b4
G4RDS <account@g4rds.jp>
2022-02-22 10:28:12 +0900

    Redirect to weekly page on access to top page

--- src/pages/TopPage.jsx
+++ src/pages/TopPage.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Header } from '../components/common/Header';
 
 const TopPage = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate('/reservations/weekly', { replace: true });
+  }, [navigate]);
+
   return (
     <div>
       <Header />
